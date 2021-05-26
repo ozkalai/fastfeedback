c1a29705e2d8321ef8890f00f752820145c57ec7
ozkalai <melihozkalayy@gmail.com>
2021-05-26 18:19:42 +0300

    landing page fixed

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,36 @@
 import Head from "next/head";
-import { useAuth } from "../lib/auth";
-import styles from "../styles/Home.module.css";
+import { useAuth } from "@/lib/auth";
+import styles from "@/styles/Home.module.css";
+import { Button, Text, Icon, Flex } from "@chakra-ui/core";
 
 export default function Home() {
   const auth = useAuth();
   console.log(auth);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
+      <Head>Fast Feedback !</Head>
+      <Icon name="logo" size="64px" color="black" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback !</h1>
-        {auth.user ? (
-          <div className={styles.description}>
-            Current user: <span>{auth?.user?.email}</span>
-          </div>
-        ) : (
-          ""
-        )}
-        {auth.user ? (
-          ""
-        ) : (
-          <button onClick={(e) => auth.signinWithGithub()}>
-            Sign In with Github
-          </button>
-        )}
-        <button onClick={(e) => auth.signinWithGoogle()}>
+      {auth.user ? (
+        ""
+      ) : (
+        <Button onClick={(e) => auth.signinWithGoogle()}>
           Sign In with Google
-        </button>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign out</button>
-        )}
-      </main>
+        </Button>
+      )}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      {auth?.user && (
+        <Button mt={4} size="sm" onClick={(e) => auth.signout()}>
+          Sign out
+        </Button>
+      )}
+    </Flex>
   );
   console.log(auth);
 }
