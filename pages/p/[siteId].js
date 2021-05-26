c0cd7ee76f3256e49f2496f7f50e58488069201d
ozkalai <melihozkalayy@gmail.com>
2021-05-26 18:20:06 +0300

    Dinamic routing

---
+++ pages/p/[siteId].js
@@ -0,0 +1,81 @@
+import { Box } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { getAllFeedback, getAllSites } from "@/lib/db-admin";
+
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const { feedback } = await getAllFeedback(siteId);
+  return {
+    props: {
+      initialFeedback: feedback,
+    },
+    revalidate: 1,
+  };
+}
+export async function getStaticPaths() {
+  const { sites } = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const FeedbackPage = ({ initialFeedback }) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const inputEl = useRef(null);
+  const [allFeedback, setAllFeedback] = useState([]);
+  useEffect(() => {
+    setAllFeedback(initialFeedback);
+  }, [initialFeedback]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text: inputEl.current.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: "pending",
+    };
+    inputEl.current.value = "";
+    setAllFeedback((currentFeedback) => [newFeedback, ...currentFeedback]);
+    createFeedback(newFeedback);
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxWidth="700px"
+      margin="0 auto"
+    >
+      {auth.user && (
+        <Box as="form" onSubmit={onSubmit}>
+          <FormControl my={8}>
+            <FormLabel htmlFor="comment">Comment</FormLabel>
+            <Input ref={inputEl} id="comment" placeholder="Leave a comment" />
+            <Button mt={4} type="submit" fontWeight="medium">
+              Add Comment
+            </Button>
+          </FormControl>
+        </Box>
+      )}
+      {allFeedback &&
+        allFeedback.map((feedback) => (
+          <Feedback
+            key={feedback.id || new Date().getTime().toString()}
+            {...feedback}
+          />
+        ))}
+    </Box>
+  );
+};
+export default FeedbackPage;
